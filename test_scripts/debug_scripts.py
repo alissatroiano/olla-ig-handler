eafeae6246a3ae026d9f63b70f68b5a90688ae10
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-09 01:22:35 -0500

    Setup and test login button loocallly

--- test_scripts/debug_scripts.py
+++ test_scripts/debug_scripts.py
@@ -1,13 +1,11 @@
-from scripts.dictionary import getCreds, make_api_call
+from dictionary import getCreds, make_api_call
 import datetime
 
 def debug_access_token(params):
     endpoint_base = dict()
     endpoint_base['input_token'] = params['access_token']
     endpoint_base['access_token'] = params['access_token']
-
     url = params['graph_domain'] + '/debug_token'
-
     return make_api_call( url, endpoint_base, params['debug'] )
 
 params = getCreds()
