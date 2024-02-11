91e0e3e789ae8bfe1cbfc713c6295d0a13c2827d
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-11 15:05:06 -0500

    Test login to ensure access token is fetched

--- my-app/src/components/Login/LoginButton.js
+++ my-app/src/components/Login/LoginButton.js
@@ -4,22 +4,24 @@ import FacebookLogin from 'react-facebook-login'
 
 
 const LoginButton = () => {
+  const [accessToken, setAccessToken] = useState("");
+
   const componentClicked = data => {
     console.log("data", data);
   };
 
 const responseFacebook = response => {
-    console.log(response);
+    console.log(response.accessToken);
   }
 
   return (
     <div>
-          Connect with Facebook
+          Login with Facebook
           <br />
           <FacebookLogin
-            appId=""
+            appId="7130922673611664"
             autoLoad={true}
-            fields="name,public_profile"
+            fields="name,public_profile,picture"
             onClick={componentClicked}
             callback={responseFacebook} 
           />
