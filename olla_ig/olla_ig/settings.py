16a90fde1f13c6bfe093f1ecb4bccfc86c1b1060
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-11 21:35:37 -0500

    Clean up dir

--- olla_ig/olla_ig/settings.py
+++ olla_ig/olla_ig/settings.py
@@ -6,8 +6,6 @@ if os.path.exists("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -113,12 +111,13 @@ WSGI_APPLICATION = 'olla_ig.wsgi.application'
 
 
 # Database
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+ }
 
 
 # Password validation
