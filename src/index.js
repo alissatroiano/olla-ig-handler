516f0a35619013816797597c89d26e975db5747a
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-13 17:02:33 -0500

    Update index.js to initializeFacebookSDK on launch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css';
+
+import App from './components/App/App';
+import reportWebVitals from "./reportWebVitals";
+import initFacebookSDK from "./initFacebookSDK";
+
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+initFacebookSDK().then(() => {
+  renderApp();
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
