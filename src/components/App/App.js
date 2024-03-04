33b448cfb07b8b0a0faee5dd51f4b51158449571
Alissa Troiano <alissatroianonyc@gmail.com>
2024-03-04 18:14:26 -0500

    Update scope for fb login for screen recording

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -136,7 +136,7 @@ function App() {
       },
       {
         scope:
-          "instagram_basic, pages_show_list, instagram_content_publish, business_management,instagram_manage_comments, instagram_manage_insights, instagram_manage_messages,pages_read_engagement, pages_manage_metadata, pages_read_user_content, pages_manage_posts",
+          "instagram_basic, pages_show_list, instagram_content_publish, instagram_manage_comments, instagram_manage_insights,pages_read_engagement, pages_manage_metadata",
       }
     );
   };
