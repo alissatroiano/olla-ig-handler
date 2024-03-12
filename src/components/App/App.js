419ffe57fc95ce832cd4c0421df5c1731b7268d9
Alissa Troiano <alissatroianonyc@gmail.com>
2024-03-12 10:54:33 -0400

    Update scope with required perrmissions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -150,7 +150,7 @@ function App() {
       },
       {
         scope:
-          "instagram_basic, instagram_manage_comments",
+          "pages_show_list, pages_read_engagement, instagram_basic, instagram_manage_comments, business_management, pages_manage_metadata",
       }
     );
   };
