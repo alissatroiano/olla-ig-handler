31dd58822d9646932525feb5e507bb8c264c8bd9
Alissa Troiano <alissatroianonyc@gmail.com>
2024-03-26 18:23:44 -0400

    Update navbar routes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,11 +3,9 @@ import logoImg from '../../assets/images/logo.png';
 
 function Logo() {
   return (
-    <div className="logoImg">
         <a href='https://www.olla.ai/' target='_blank' rel="noreferrer">
       <img className="logoImg" src={logoImg} alt="Logo" />
       </a>
-    </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,13 +4,14 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PrivacyPolicy from "../PrivacyPolicy/PrivacyPolicy";
+import Logo from "../Logo/Logo"
 
 function NavbarNav() {
   return (
     <Router>
       <Navbar bg="transparent" data-bs-theme="light" className='shadow-lg'>
         <Container>
-          <Navbar.Brand as={Link} to="/">OLLA AI</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/"><Logo id="navLogo" /></Navbar.Brand>
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/privacyPolicy">Privacy Policy</Nav.Link>
